bd4a95ed824f0bc803d53db42c07a5e107d13fd0
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-16 11:05:08 +0200

    select color from list

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
 const widthInput = document.getElementById('width');
 const heightInput = document.getElementById('height');
 const resetButton = document.getElementById('reset');
+const colorList = document.querySelector('.color-list');
 const canvasEl = document.querySelector('.canvas');
 
 let width = parseInt(widthInput.value);
 let height = parseInt(heightInput.value);
 let canvasSize = width * height;
+
+const colors = [
+    '#000000', '#FFFFFF', '#FC9A9C', '#ED3B3C', '#B01A1A', '#FEB168', '#F86817', '#BB390F', '#FDCC44', '#F3940E', '#AB490D'
+];
+const colorItemEls = [];
+const colorDivEls = [];
+
+colors.forEach(function(color) {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    
+    li.classList.add('color-item');
+    div.classList.add('color-item__color');
+    div.style.backgroundColor = color;
+
+    li.append(div);
+    colorList.append(li);
+
+    colorItemEls.push(li);
+    colorDivEls.push(div);
+});
 
 createCanvas();
 
@@ -26,6 +48,15 @@ canvasEl.addEventListener('click', function(event) {
     if (target !== canvasEl) {
         target.style.border = 'none';
         target.style.backgroundColor = '#000000';
+    }
+});
+
+colorList.addEventListener('click', function(event) {
+    const target = event.target;
+    const bgColor = target.style.backgroundColor;
+    
+    if (target !== colorList) {
+        console.log(target, bgColor);
     }
 });
 
