595e930bb0376a02dd5cf33972c0d252ee60fc48
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-18 11:35:52 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const widthInput = document.getElementById('width');
 const heightInput = document.getElementById('height');
 const resetButton = document.getElementById('reset');
-const colorList = document.querySelector('.color-list');
+const colorListEl = document.querySelector('.color-list');
 const canvasEl = document.querySelector('.canvas');
 
 let width = parseInt(widthInput.value);
@@ -11,9 +11,8 @@ let canvasSize = width * height;
 const colors = [
     '', '#000000', '#FFFFFF', '#FC9A9C', '#ED3B3C', '#B01A1A', '#FEB168', '#F86817', '#BB390F', '#FDCC44', '#F3940E', '#AB490D'
 ];
-let selectedColor, selectedListItem;
+let selectedColor, selectedListItem, isClicked = false;
 const colorItemEls = [];
-// const colorDivEls = [];
 
 colors.forEach(function(color, index) {
     const li = document.createElement('li');
@@ -24,8 +23,11 @@ colors.forEach(function(color, index) {
     div.style.backgroundColor = color;
 
     if (index === 0) {
-        li.classList.add('selected');
         div.classList.add('erase');
+    }
+
+    if (index === 1) {
+        li.classList.add('selected');
 
         selectedColor = color;
         selectedListItem = li;
@@ -36,10 +38,9 @@ colors.forEach(function(color, index) {
     }
 
     li.append(div);
-    colorList.append(li);
+    colorListEl.append(li);
 
     colorItemEls.push(li);
-    // colorDivEls.push(div);
 });
 
 createCanvas();
@@ -52,54 +53,36 @@ resetButton.addEventListener('click', function() {
     canvasSize = width * height;
 
     createCanvas();
-    // console.log(width, height);
 });
 
-canvasEl.addEventListener('click', function(event) {
-    const target = event.target;
-
-    if (target !== canvasEl) {
-        if (selectedColor === '') {
-            target.style.border = '1px solid #e2e2e2';
-            target.style.backgroundColor = '#F1F1F1';
-        }
-        else {
-            target.style.border = 'none';
-            target.style.backgroundColor = selectedColor;
-        }
+canvasEl.addEventListener('click', (event) => {
+    if (event.target === canvasEl) {
+        return;
     }
+
+    draw(event);
 });
 
-let isClicked = false;
-canvasEl.addEventListener('mousedown', function(event) {
+canvasEl.addEventListener('mousedown', (event)  => {
     if (event.button === 0) {
         isClicked = true;
     }
 });
-canvasEl.addEventListener('mousemove', function(event) {
+
+canvasEl.addEventListener('mousemove', (event) => {
     if (isClicked) {
-        const target = event.target;
-    
-        if (target !== canvasEl) {
-            if (selectedColor === '') {
-                target.style.border = '1px solid #e2e2e2';
-                target.style.backgroundColor = '#F1F1F1';
-            }
-            else {
-                target.style.border = 'none';
-                target.style.backgroundColor = selectedColor;
-            }
-        }
+        draw(event);
     }
 });
-canvasEl.addEventListener('mouseup', function() {
+
+canvasEl.addEventListener('mouseup', () => {
     isClicked = false;
 });
 
-colorList.addEventListener('click', function(event) {
+colorListEl.addEventListener('click', function(event) {
     const target = event.target;
     
-    if (target !== colorList && !colorItemEls.includes(target)) {
+    if (target !== colorListEl && !colorItemEls.includes(target)) {
         selectedListItem.classList.remove('selected');
 
         selectedListItem = target.parentElement;
@@ -121,4 +104,15 @@ function createCanvas() {
 
 function resetCanvas() {
     canvasEl.innerHTML = '';
+}
+
+function draw(e) {
+    if (!selectedColor) {
+        e.target.style.border = '1px solid #E2E2E2';
+        e.target.style.backgroundColor = '#F1F1F1';
+    }
+    else {
+        e.target.style.border = 'none';
+        e.target.style.backgroundColor = selectedColor;
+    }
 }
